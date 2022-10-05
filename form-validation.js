fbbc9fa7ae349b1661beb2c633133ff3473b1232
Alena Nemtsova <jelenatbs@gmail.com>
2022-10-05 16:11:54 +0300

    added validation for name and mobile phone fields

--- form-validation.js
+++ form-validation.js
@@ -1,31 +1,51 @@
 
-let firstname = document.querySelector('#firstname');
-let lastname = document.querySelector('#lastname');
-let mobileEmail = document.querySelector('#mobileEmail');
-let password = document.querySelector('#password');
+const firstname = document.querySelector('#firstname');
+const lastname = document.querySelector('#lastname');
+const mobileEmail = document.querySelector('#mobileEmail');
+const password = document.querySelector('#password');
 
-let form = document.querySelector('.form');
-let error = document.querySelector('#errorMessage');
+const form = document.querySelector('.form');
+const error = document.querySelector('#errorMessage');
+
+const firstnameError = document.querySelector('span.error-firstname');
+const lastnameError = document.querySelector('span.error-lastname');
+const mobileEmailError = document.querySelector('span.error-mobileEmail');
 
 
-form.onsubmit = function(evt) {
-    error.innerHTML = "";
-
-    if(firstname.value == '' || lastname.value == '') {
+form.onsubmit = function validate(evt) {
+    if (firstname.validity.valueMissing) {
         evt.preventDefault();
-        error.innerHTML += "What's your name?</br>";
-    }
-    
-    if(mobileEmail.value == '') {
-        evt.preventDefault();
-        error.innerHTML += "Enter your mobile number or email</br>";
+        firstnameError.textContent = "What's your name?";
     }
 
-    if(password.value == '') {
+    if (lastname.validity.valueMissing) {
         evt.preventDefault();
-        error.innerHTML += "Create a password</br>";
+        lastnameError.textContent = "What's your name?";
     }
-}; 
+
+    const mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    const mobileFormat = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/; //для телефонов РФ
+    if (mobileEmail.value.match(mailFormat) || mobileEmail.value.match(mobileFormat)) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        mobileEmailError.textContent = "Please enter a valid mobile number or email address.";
+    }
+
+};
+
+firstname.onfocus = function () {
+    firstnameError.textContent = '';
+};
+
+lastname.onfocus = function () {
+    lastnameError.textContent = '';
+};
+
+mobileEmail.onfocus = function () {
+    mobileEmailError.textContent = '';
+};
 
 
 
