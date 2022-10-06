7927526a57b2eaf4f60e56a80dbdc7794d8b42e1
Alena Nemtsova <jelenatbs@gmail.com>
2022-10-06 17:14:56 +0300

    added validation for password field

--- form-validation.js
+++ form-validation.js
@@ -10,42 +10,63 @@ const error = document.querySelector('#errorMessage');
 const firstnameError = document.querySelector('span.error-firstname');
 const lastnameError = document.querySelector('span.error-lastname');
 const mobileEmailError = document.querySelector('span.error-mobileEmail');
+const passwordError = document.querySelector('span.error-password');
 
 
 form.onsubmit = function validate(evt) {
     if (firstname.validity.valueMissing) {
         evt.preventDefault();
         firstnameError.textContent = "What's your name?";
+        firstname.classList.add('error-field');
     }
 
     if (lastname.validity.valueMissing) {
         evt.preventDefault();
         lastnameError.textContent = "What's your name?";
+        lastname.classList.add('error-field');
     }
 
-    const mailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
-    const mobileFormat = /^\+?[78][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/; //для телефонов РФ
-    if (mobileEmail.value.match(mailFormat) || mobileEmail.value.match(mobileFormat)) {
+    const emailFormat = /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i;
+    const mobileFormat = /^\+?\d{1,3}?[- .]?\(?(?:\d{2,3})\)?[- .]?\d\d\d[- .]?\d\d[- .]?\d\d$/; //для любых телефонов от 10 цифр
+    if (mobileEmail.value.match(emailFormat) || mobileEmail.value.match(mobileFormat)) {
         return true;
     }
     else {
         evt.preventDefault();
         mobileEmailError.textContent = "Please enter a valid mobile number or email address.";
+        mobileEmail.classList.add('error-field');
+    }
+
+    const passwordFormat = /^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$/; //пароль вида AA#11aaa
+    if (password.value.match(passwordFormat)) {
+        return true;
+    }
+    else {
+        evt.preventDefault();
+        passwordError.textContent = "Enter a conbination of at least eight characters including two numbers and a punctuation mark.";
+        passwordError.classList.add('error-field');
     }
 
 };
 
 firstname.onfocus = function () {
     firstnameError.textContent = '';
+    firstname.classList.remove('error-field');
 };
 
 lastname.onfocus = function () {
     lastnameError.textContent = '';
+    lastname.classList.remove('error-field');
 };
 
 mobileEmail.onfocus = function () {
     mobileEmailError.textContent = '';
+    mobileEmail.classList.remove('error-field');
 };
 
+password.onfocus = function () {
+    passwordError.textContent = '';
+    password.classList.remove('error-field');
+};
 
 
